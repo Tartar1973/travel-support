ed08ab81d8f8177f56ea743581e8ef23a36a9ab6
Tartar1973 <arata_tsutsui@hotmail.com>
2026-04-03 00:05:12 +0900

    Add minimum Next.js pages entry

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "sans-serif" }}>
+      <h1>travel-support-clean</h1>
+      <p>Next.js minimum working page</p>
+    </main>
+  );
+}
